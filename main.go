5deb52e46b7a656abc83f6f09db2fbcce6d1ec5d
zlatabee <zlatabee@gmail.com>
2020-02-23 21:40:56 -0500

    populate struct with info about buckets & events

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type Data struct {
+	App       string
+	Url       string
+	Title     string
+	Type      string
+	Duration  float64
+	Timestamp string
+	Audible   bool
+	Incognito bool
+}
+
+type Event struct {
+	Timestamp string
+	Duration  float64
+	Data      Data
+}
+
+type Bucket struct {
+	Id       string
+	Created  string
+	Name     string
+	Type     string
+	Client   string
+	Hostname string
+	Events   []Event
+}
+
+type Export struct {
+	Buckets map[string]Bucket
+}
+
 func main() {
-	resp, err := http.Get("http://localhost:5600/api/0/info")
+	resp, err := http.Get("http://localhost:5600/api/0/export")
 	if err != nil {
 		log.Fatalf("Error oh no: %v", err)
 	}
@@ -16,6 +48,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("Another error: %v", err)
 	}
-	log.Printf("Server info: %s", respBody)
-	log.Println("Hi")
+
+	var export Export
+	if err := json.Unmarshal(respBody, &export); err != nil {
+		log.Fatalf("Uh oh: %v", err)
+	}
+	log.Println(export.Buckets)
 }
