cbec1995002ff121c5a2ae8fdc50ca554135fe67
zlatabee <zlatabee@gmail.com>
2020-02-26 15:26:11 -0500

    generate timeperiods string programmatically instead of hardcoding

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type Data struct {
@@ -34,9 +35,6 @@ type Request struct {
 }
 
 func main() {
-	/*jsonStr := `{"query":["window_events = query_bucket(find_bucket('aw-watcher-window_'));","not_afk_events = query_bucket(find_bucket('aw-watcher-afk_'));","not_afk_events = filter_keyvals(not_afk_events, \"status\", [\"not-afk\"]);","events = filter_period_intersect(window_events, not_afk_events);","classes = [[[\"Work\"],{\"type\":\"regex\",\"regex\":\"Google Docs\"}],[[\"Work\",\"Programming\"],{\"type\":\"regex\",\"regex\":\"GitHub|Stack Overflow\"}],[[\"Work\",\"Programming\",\"ActivityWatch\"],{\"type\":\"regex\",\"regex\":\"ActivityWatch|aw-\",\"ignore_case\":true}],[[\"Media\",\"Games\"],{\"type\":\"regex\",\"regex\":\"Minecraft|RimWorld\"}],[[\"Media\",\"Video\"],{\"type\":\"regex\",\"regex\":\"YouTube|Plex\"}],[[\"Media\",\"Social Media\"],{\"type\":\"regex\",\"regex\":\"reddit|Facebook|Twitter|Instagram\",\"ignore_case\":true}],[[\"Comms\",\"IM\"],{\"type\":\"regex\",\"regex\":\"Messenger|Telegram|Signal|WhatsApp\"}],[[\"Comms\",\"Email\"],{\"type\":\"regex\",\"regex\":\"Gmail\"}]];","events = categorize(events, classes);","events = merge_events_by_keys(events,[\"$category\"]);","RETURN = events;",";"],"timeperiods":["2020-02-25T00:00:00-05:00/2020-02-26T00:00:00-05:00"]}
-	  `*/
-
 	queryStr := `
   window_events = query_bucket(find_bucket('aw-watcher-window_'));
   not_afk_events = query_bucket(find_bucket('aw-watcher-afk_'));
@@ -51,8 +49,14 @@ func main() {
   RETURN = events;
   `
 
+	ny, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		log.Fatalf("Error loading New York location: %v", err)
+	}
+	timeperiods := time.Now().Add(-24*time.Hour).In(ny).Format(time.RFC3339) + "/" + time.Now().In(ny).Format(time.RFC3339)
+
 	query := strings.SplitAfter(queryStr, ";")
-	req := Request{Query: query, Timeperiods: []string{"2020-02-26T00:00:00-05:00/2020-02-27T00:00:00-05:00"}}
+	req := Request{Query: query, Timeperiods: []string{timeperiods}}
 	reqBytes, err := json.Marshal(req)
 	if err != nil {
 		log.Fatalf("Error aaaaaaaa: %v", err)
